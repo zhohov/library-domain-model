25e141bfd3c38c10c19fec50cc676af7dfd1b800
maksimzhohov <zhohovmaksim@gmail.com>
2024-07-16 16:45:29 +0300

    feat(value-objects): add validation for Address

--- app/src/domain/value_objects.py
+++ app/src/domain/value_objects.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+from src.exceptions import EmptyValueError, InvalidTextLengthError
+
 
 @dataclass(frozen=True)
 class BaseValueObject:
@@ -12,7 +14,22 @@ class Address(BaseValueObject):
     street: str
     postcode: str
 
-    def __post_init__(self, ) -> None:
+    def validate_empty_fields(self) -> None:
         if not self.city:
-            raise ValueError("City cannot be empty")
+            raise EmptyValueError(value="city")
+        if not self.street:
+            raise EmptyValueError(value="street")
+        if not self.postcode:
+            raise EmptyValueError(value="postcode")
+    
+    def validate_length(self) -> None:
+        if len(self.city) > 255 or len(self.city) < 3:
+             raise InvalidTextLengthError(value="city")
+        if len(self.street) > 255 or len(self.street) < 3:
+            raise InvalidTextLengthError(value="street")
+        if len(self.postcode) != 6:
+            raise InvalidTextLengthError(value="postcode")
 
+    def __post_init__(self, ) -> None:
+        self.validate_empty_fields()
+        self.validate_length()
